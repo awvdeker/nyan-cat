701d643d3acb32245df0527a5aad3d2c6ec850b7
awvdeker <alexander.vandekerckhove@gmail.com>
2019-03-27 17:05:58 +0100

    veel gesukkel met Date(), juiste manier zie comments

--- index.js
+++ index.js
@@ -4,22 +4,42 @@ knop.addEventListener("click",catRain);
 
 function catRain () {
 
+
+  //setInterval om de 1 seconde een katje maken
+
+
+  oneCat();
+
+
+  //setTimeout zet op 10 seconden,
+  //en opdracht: clearInterval
+
+
+
+}
+
+function oneCat () {
   var kat=document.createElement("img");
   document.body.appendChild(kat);
 
   kat.src="nyancat.png";
-  kat.id="nyan-cat";
+  //kat.id="nyan-cat";
 
   kat.style.position = "fixed";
-  kat.style.left = "0";
-  kat.style.top = "0";
-  kat.style.transition = "all 3s linear";
 
-  setTimeout(function(){
+    kat.style.left = "0";
+      kat.style.top = "0";
+      kat.style.transition = "all 1s linear";
 
-    kat.style.left = "100vw";
-    kat.style.top = "100vh";
 
-  }, 500);
+    setTimeout(function(){
+
+      kat.style.left = "100vw";
+      kat.style.top = "100vh";
+
+    }, 100);
 
 }
+
+//als speciale achtergrond neem animated
+// gif van het internet van nyan cats
