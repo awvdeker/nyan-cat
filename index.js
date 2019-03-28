7afd5ce33fee06699e0b53211a4eb4c33ae83ef3
awvdeker <alexander.vandekerckhove@gmail.com>
2019-03-28 10:18:27 +0100

    nyan cat background and button disappears

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ knop.addEventListener("click",catRain);
 
 function catRain () {
 
+  // background nyan cat en button diplay: none
+
+  document.body.style.backgroundImage= "url('background.gif')";
+  document.body.style.backgroundRepeat= "no-repeat";
+  document.body.style.backgroundSize="cover";
+
+  document.getElementById("knop").style.display="none";
+
+
   //setInterval om de 1 seconde een katje maken
 
   var herhaling = setInterval(oneCat,1000);
@@ -13,6 +22,15 @@ function catRain () {
 
   setTimeout(function(){
     clearInterval(herhaling);
+
+  //background normaal en display button
+
+  document.body.style.backgroundImage= "none";
+
+  document.getElementById("knop").style.display="block";
+
+
+
   },10000);
 
 }
@@ -26,17 +44,17 @@ function oneCat () {
 
   kat.style.position = "fixed";
 
-    kat.style.left = "0";
-      kat.style.top = "0";
-      kat.style.transition = "all 2s linear";
+  kat.style.left = "0";
+  kat.style.top = "0";
+  kat.style.transition = "all 2s linear";
 
 
-    setTimeout(function(){
+  setTimeout(function(){
 
-      kat.style.left = "100vw";
-      kat.style.top = "100vh";
+    kat.style.left = "100vw";
+    kat.style.top = "100vh";
 
-    }, 100);
+  }, 100);
 
 }
 
